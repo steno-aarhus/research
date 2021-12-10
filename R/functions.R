19b32bb20252bfbf49467cd0aa4228ce6fdc49cd
Luke W. Johnston <lwjohnst@gmail.com>
2021-12-10 15:22:33 +0100

    code to create small images of researchers

--- R/functions.R
+++ R/functions.R
@@ -30,3 +30,31 @@ rmd_to_update %>%
     pull(file_id) %>%
     path_ext_set("Rmd") %>%
     map(rmarkdown::render)
+
+# To insert images of researchers -----------------------------------------
+
+library(rvest)
+library(tidyverse)
+
+sdca_research_page <- read_html("https://www.stenoaarhus.dk/research/")
+
+researchers <- sdca_research_page %>%
+    html_elements(".headline") %>%
+    html_elements("a")
+
+researcher_names <- researchers %>%
+    html_text2()
+
+researcher_links <- researchers %>%
+    html_attr("href")
+
+researcher_image_location <- sdca_research_page %>%
+    html_elements("img") %>%
+    html_attr("src") %>%
+    str_subset(".*(kontakt|research).*")
+
+# To test if lengths are same.
+# list(researcher_names, researcher_links, researcher_image_location) %>%
+#     map_int(~length(.x))
+
+glue::glue(g)
